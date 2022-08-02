800467a9e7960a93a39421953a5c67d5bac31c01
a.sharifi <a.sharifi@ncdrc.info>
2022-08-02 11:53:30 +0330

    inhertance Rule Class

--- rules/rule_classes.py
+++ rules/rule_classes.py
@@ -6,21 +6,26 @@ from rules.operation import define_operation
 import pandas as pd
 
 
-class Operation:
+class Rule:
     def __init__(self, config, data):
-        self.types = ['op_sum', 'op_multiply', 'op_subtract', 'op_division', 'op_sqrt', 'op_power']
         self.config = config
         self.data = data
         self.type_checker = 'O'
 
     def find_configs(self):
-        op_par = self.types
-        self.config = self.config[self.config.type.isin(op_par)]
+        par = self.types
+        self.config = self.config[self.config.type.isin(par)]
+
+
+class Operation(Rule):
+    def __init__(self, config, data):
+        super().__init__(config, data)
+        self.types = ['op_sum', 'op_multiply', 'op_subtract', 'op_division', 'op_sqrt', 'op_power']
 
     def validate_config(self):
         self.config['valid_type'] = self.config['col'].apply(lambda x:
-                                                              True if (self.data[x].dtypes != self.type_checker)
-                                                              else False)
+                                                             True if (self.data[x].dtypes != self.type_checker)
+                                                             else False)
 
         self.config = self.config[self.config.valid_type]
 
@@ -28,21 +33,15 @@ class Operation:
         if len(self.config) > 0:
             # apply len rule
             operation_rule = list(map(define_operation, [(self.data.loc[:, [row['col']]], row)
-                                                            for index, row in self.config.iterrows()]))
+                                                         for index, row in self.config.iterrows()]))
             operation_rules = pd.concat(operation_rule, axis=1)
             self.data = operation_rules
 
 
-class FiledsOpertion:
+class FiledsOpertion(Rule):
     def __init__(self, config, data):
+        super().__init__(config, data)
         self.types = ['sum', 'multiply', 'subtract', 'division']
-        self.config = config
-        self.data = data
-        self.type_checker = 'O'
-
-    def find_configs(self):
-        fields_op_par = self.types
-        self.config = self.config[self.config.type.isin(fields_op_par)]
 
     def validate_config(self):
         self.config['valid_type1'] = self.config['col'].apply(lambda x:
@@ -66,12 +65,11 @@ class FiledsOpertion:
             self.data = fields_rules
 
 
-class RangeRule:
+class RangeRule(Rule):
     def __init__(self, config, data):
+        super().__init__(config, data)
         self.types = ['range', 'range_e', 'range_l', 'range_g', 'g', 'l', 'ge', 'le']
-        self.type_checker = 'O'
-        self.config = config
-        self.data = data
+
 
     def find_configs(self):
         range_rule_par = self.types
@@ -94,16 +92,10 @@ class RangeRule:
             self.data = range_rules
 
 
-class LengthRule:
+class LengthRule(Rule):
     def __init__(self, config, data):
+        super().__init__(config, data)
         self.types = ['len', 'len_g', 'len_l', 'len_ge', 'len_le']
-        self.type_checker = 'O'
-        self.config = config
-        self.data = data
-
-    def find_configs(self):
-        len_rule_par = self.types
-        self.config = self.config[self.config.type.isin(len_rule_par)]
 
     def validate_config(self):
         self.config['valid_type'] = self.config['col'].apply(lambda x:
