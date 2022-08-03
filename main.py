af92e9d535d597e15a6f755bbe03df31e3f85133
a.sharifi <a.sharifi@ncdrc.info>
2022-08-03 08:09:08 +0330

    add comments in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from rules.rule_classes import RangeRule, LengthRule, FiledsOpertion, Operation , BoolianOperator
 import pandas as pd
 
+# make sample data
 df = pd.DataFrame({'age': [10, 12, 26, 5, 67, 8, 99, 100],
                    'value': [10, 12, 26, 5, 67, 85, 99, 100],
                    'nid': ['100000000', '02023240506', '122949214949',
@@ -8,35 +9,38 @@ df = pd.DataFrame({'age': [10, 12, 26, 5, 67, 8, 99, 100],
                            '99990020021020102', '0000001221312',
                            '99990020021020102']})
 
+# read rules config
 rules = pd.read_json(path_or_buf='./configs/rules.json', orient='records')
-# rules = pd.read_csv('./configs/rules.csv')
 print(rules.head(5))
 
-
+# define class object
 len_rule_obj = LengthRule(config=rules, data=df)
 range_rule_obj = RangeRule(config=rules, data=df)
 field_rule_obj = FiledsOpertion(config=rules, data=df)
 op_rule_obj = Operation(config=rules, data=df)
 
-
+# lenght rules
 len_rule_obj.find_configs()
 len_rule_obj.validate_config()
 len_rule_obj.apply_rules()
 df = pd.concat([df, len_rule_obj.data], axis=1)
 print(df)
 
+# range rules
 range_rule_obj.find_configs()
 range_rule_obj.validate_config()
 range_rule_obj.apply_rules()
 df = pd.concat([df, range_rule_obj.data], axis=1)
 print(df)
 
+# field rules
 field_rule_obj.find_configs()
 field_rule_obj.validate_config()
 field_rule_obj.apply_rules()
 df = pd.concat([df, field_rule_obj.data], axis=1)
 print(df)
 
+# operation rules
 op_rule_obj.find_configs()
 op_rule_obj.validate_config()
 op_rule_obj.apply_rules()
@@ -44,6 +48,7 @@ df = pd.concat([df, op_rule_obj.data], axis=1)
 print(df)
 
 
+# bool operation rules
 bool_rule_obj = BoolianOperator(config=rules, data=df)
 bool_rule_obj.find_configs()
 bool_rule_obj.validate_config()
@@ -51,4 +56,5 @@ bool_rule_obj.apply_rules()
 df = pd.concat([df, bool_rule_obj.data], axis=1)
 print(df)
 
+# save data
 df.to_csv('result.csv', index=False)
